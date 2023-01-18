3637ef069161be6fd589ec78a529a87a363413db
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2023-01-18 13:52:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,89 @@
-module "gcloud" {
-  source  = "terraform-google-modules/gcloud/google"
-  version = "~> 2.0"
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
 
-  platform = "linux"
-  additional_components = ["kubectl", "beta"]
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
 
-  create_cmd_entrypoint  = "gcloud"
-  create_cmd_body        = "container clusters create cicd-cluster --machine-type=g1-small --num-nodes=3 --zone=us-central1-a"
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = "alert-flames-286515"
+  name                       = "cicd-cluster"
+  region                     = "us-central1"
+  regional                   = false
+  zones                      = ["us-central1-a"]
+  network                    = "default"
+  subnetwork                 = "default"
+  ip_range_pods              = ""
+  ip_range_services          = ""
+  http_load_balancing        = true
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+
+  node_pools = [
+    {
+      name               = "default-node-pool"
+      machine_type       = "e2-medium"
+      min_count          = 1
+      max_count          = 3
+      local_ssd_count    = 0
+      spot               = false
+      disk_size_gb       = 100
+      disk_type          = "pd-standard"
+      image_type         = "COS_CONTAINERD"
+      enable_gcfs        = false
+      enable_gvnic       = false
+      auto_repair        = true
+      auto_upgrade       = true
+      service_account    = "tf-gke-cicd-cluster-823y@alert-flames-286515.iam.gserviceaccount.com"
+      preemptible        = false
+      initial_node_count = 1
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
+  }
 }
